c3d9b376a19002bdf3b48ce057c99dfd1a62082e
Akinkunmi Mustapha <akinkunmimustapha1@gmail.com>
2026-01-27 00:33:26 +0000

    dev

--- src/services/tagging_service/application/tagging_service_builder.py
+++ src/services/tagging_service/application/tagging_service_builder.py
@@ -9,7 +9,7 @@ from datetime import UTC, datetime
 from src.shared.database.client import SQLModelClient
 from src.services.tagging_service.domain.models.models import Item, Item_tag, Tag
 from src.services.tagging_service.infrastructure.repositories.interface import BaseRepository
-from src.services.ingestion_service.infrastructure.repositories.entity_repository import EntityRepository
+from src.shared.repositories.entity_repository import EntityRepository
 from src.services.tagging_service.infrastructure.repositories.repositories import DomainRepositoryFactory
 from src.shared.repositories.query_repository import ItemSQLQueryRepository
 from src.shared.utils.custom_logger import customer_logger
@@ -23,8 +23,8 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 def build_tagging_service():
   database_client = SQLModelClient(database_url=DATABASE_URL)
-  item_repo = DomainRepositoryFactory.get_repository("item", EntityRepository("asset", client=database_client))
-  tag_repo = DomainRepositoryFactory.get_repository("tag", EntityRepository("tag", client=database_client))
-  item_tag_repo = DomainRepositoryFactory.get_repository("item_tag", EntityRepository("asset_tag", client=database_client))
+  item_repo = DomainRepositoryFactory.get_repository("item", EntityRepository("asset"))
+  tag_repo = DomainRepositoryFactory.get_repository("tag", EntityRepository("tag"))
+  item_tag_repo = DomainRepositoryFactory.get_repository("item_tag", EntityRepository("asset_tag"))
   item_query_repo = ItemSQLQueryRepository(database_client)
   return TaggingService(item_repo, tag_repo, item_tag_repo, item_query_repo)
