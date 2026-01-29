416ae343dfe6761bd8c54af75d7a5b571abfe3d6
Akinkunmi Mustapha <akinkunmimustapha1@gmail.com>
2026-01-29 19:46:37 +0000

    portfolio_page_01

--- src/dashboard/src/pages/portfolio/charts.py
+++ src/dashboard/src/pages/portfolio/charts.py
@@ -5,22 +5,22 @@ import plotly.express as px
 class WinnersPlotlyBarChart:
     def render(self, data):
         df = pd.DataFrame(data)
-        df = df.sort_values("profit", ascending=False).head(5)
+        df = df.sort_values("profit", ascending=False).head(10)
 
         fig = px.bar(
             df,
             x="name",
             y="profit",
-            title="Top 5 Winners",
+            title="Top 10 Winners",
             text="profit",
         )
 
         fig.update_traces(
             marker=dict(
-                color="#2ecc71",
+                color="#33985D",
                 line=dict(width=0),
             ),
-            width=0.35,  # 👈 slimmer bars
+            width=0.35,
             texttemplate="%{text:.2f}",
             textposition="outside",
         )
@@ -45,22 +45,22 @@ class WinnersPlotlyBarChart:
 class LosersPlotlyBarChart:
     def render(self, data):
         df = pd.DataFrame(data)
-        df = df.sort_values("profit", ascending=True).head(5)
+        df = df.sort_values("profit", ascending=True).head(10)
 
         fig = px.bar(
             df,
             x="name",
             y="profit",
-            title="Top 5 Losers",
+            title="Top 10 Losers",
             text="profit",
         )
 
         fig.update_traces(
             marker=dict(
-                color="#e74c3c",
+                color="#A73528",
                 line=dict(width=0),
             ),
-            width=0.35,  # 👈 slimmer bars
+            width=0.35,
             texttemplate="%{text:.2f}",
             textposition="outside",
         )
@@ -95,15 +95,15 @@ class PortfolioPerformancePlotlyLineChart:
 
         fig.update_traces(
             line=dict(
-                width=1,
-                color="#34495e",
+                width=1.5,
+                color="#2483cc",
             ),
             mode="lines",
         )
 
         fig.update_layout(
             template="plotly_white",
-            height=350,
+            height=550,
             hovermode="x unified",
             margin=dict(l=20, r=20, t=20, b=20),
             xaxis_title=None,
